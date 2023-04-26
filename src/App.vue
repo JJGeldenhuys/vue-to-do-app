393f1d90e0abace885a90dee71a0fdc968d22b02
jgeldenhuys <joa.geldenhuys@gmail.com>
2023-04-26 13:42:24 +0200

    Ui functioning

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <searchBar/>
+    <v-main>
+      <ToDoForm/>
+      <ToDoItem/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+import ToDoForm from './components/ToDoForm'
+import ToDoItem from './components/ToDoItem'
+import searchBar from './components/SearchBar'
+
+export default 
+{
   name: 'App',
+
   components: {
-    HelloWorld
+    searchBar,
+    ToDoForm,
+    ToDoItem,
+    
+  },
+
+  data() {
+  //   return {
+  //   ToDoItems:[
+  //   {todo_id:1, title:'Buy bread', description: 'Remember to go to the shops to buy bread', date:'2022-01-01', categories: ['Shopping','Groceries','Food']},
+  //   {todo_id:2, title:'Buy milk', description: 'Remember to go to the shops to buy milk', categories: ['Shopping','Groceries','Drinks']},
+  //   {todo_id:3, title:'Fill up car', description: 'The car uses Unleaded 95', date:'2022-01-01', categories: ['Fuel']},
+  //   {todo_id:4, title:'Pay the ISP', date:'2022-01-01', categories: null},]
+  //   };
+  // },
+  // methods: {
+  //   //addToDo(toDoLabel) {
+  //     //this.ToDoItems.push({
+  //     //});
+  //   },
+  //   updateDoneStatus(toDoId) {
+  //     const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId);
+  //     toDoToUpdate.done = !toDoToUpdate.done;
+  //   },
+  //   deleteToDo(toDoId) {
+  //     const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
+  //     this.ToDoItems.splice(itemIndex, 1);
+  //     this.$refs.listSummary.focus();
+  //   },
+  // },
+  // computed: {
+  //   listSummary() {
+  //     const numberFinishedItems = this.ToDoItems.filter(
+  //       (item) => item.done
+  //     ).length;
+  //     return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`;
+  //   },
+  // };
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="todo_id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{ title }}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete
+        <span class="visually-hidden">{{ title }}</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'ToDoItem',
+  props: {
+    todo_id: { required: true, type: String },
+    title: { required: true, type: String },
+    description: { required: false, type: String },
+    Date: { required: false, type: Date },
+    category: { required: false, type: String },
+  },
+  data() {
+    return {
+      isEditing: false,
+    };
+  },
+  computed: {
+    isDone() {
+      return this.done;
+    },
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+  },
+};
+</script>
